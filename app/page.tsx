99bceb4be38c4915474be5d0aaf69d3c034940d5
Yousran <yusranmazidan@gmail.com>
2024-12-17 22:28:12 +0800

    FEAT: Add theme provider and toggle component for dark mode support, update global styles, and modify layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import ProfileSection from '@/components/ui/profile-section';
 import ProfileContact from '@/components/ui/profile-contact';
 import Starfield from 'react-starfield';
+import { ModeToggle } from '@/components/ui/theme-toggle';
 
 export default function Home() {
   return (
-    
-    <div className='h-screen w-screen flex justify-center items-center bg-primary-foreground'>
+    <div className='h-screen w-screen flex justify-center items-center bg-primary-foreground relative'>
       <Starfield
         starCount={500}
         starColor={[255, 255, 255]}
         speedFactor={0.03}
       />
+      <div className='absolute top-4 right-4 z-50'>
+        <ModeToggle />
+      </div>
       <div className='text-center flex flex-col'>
         <ProfileSection />
         <ProfileContact />
