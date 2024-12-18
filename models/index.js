15b3947bc60304a0cce5e083032904e2c0496d38
Yousran <yusranmazidan@gmail.com>
2024-12-18 11:56:35 +0800

    FIX: Add configuration file for database settings and update Sequelize initialization to use environment-specific configurations

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 import Sequelize from 'sequelize';
+import config from '../config/config.json';
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: console.log
-});
+const env = process.env.NODE_ENV || 'development';
+const sequelizeConfig = config[env];
+
+const sequelize = new Sequelize(sequelizeConfig);
 
 export default sequelize;
