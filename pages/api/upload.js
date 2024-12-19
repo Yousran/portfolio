c35e1b35e4ec41d6a0ef8191c862e98d48b5965b
Yousran <yusranmazidan@gmail.com>
2024-12-19 14:20:07 +0800

    FIX: Enhance file upload handling with dynamic directory creation and improved error logging

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,15 +1,24 @@
 import multer from 'multer';
 import sharp from 'sharp';
 import path from 'path';
+import fs from 'fs';
 import File from '../../models/file.js';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(process.cwd(), 'public/uploads'));
+    const uploadPath = path.join(process.cwd(), 'public/uploads');
+    console.log('Upload Path:', uploadPath);
+
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath, { recursive: true });
+    }
+
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     const extension = path.extname(file.originalname);
+    console.log('Generated Filename:', uniqueSuffix + extension);
     cb(null, uniqueSuffix + extension);
   }
 });
@@ -25,6 +34,7 @@ export const config = {
 const handler = async (req, res) => {
   upload.single('file')(req, res, async (err) => {
     if (err) {
+      console.error('Upload File Error: ',err);
       return res.status(500).json({ error: 'Failed to upload file' });
     }
 
